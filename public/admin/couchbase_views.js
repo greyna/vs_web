7f993fb8016090e5f121df1a03d8974092e88bda
Arthur Van Ceulen <vandekeu@gmail.com>
2014-05-25 02:07:13 +0200

    oubli couchbase_views.js

---
+++ public/admin/couchbase_views.js
@@ -0,0 +1,26 @@
+// couchbase_views.js
+
+/**** Dev Design document name : _design/dev_component/
+	  Production Design document name : _design/component ****/
+
+
+// View name v_all_components
+function (doc, meta) {
+  if (meta.type == "json" && doc.lang && doc.type && (doc.published || doc.published == false)) {
+    emit([doc.lang, doc.type, doc.published], null);
+  }
+}
+
+// View name v_component
+function (doc, meta) {
+  if (meta.type == "json" && doc.lang && doc.type && (doc.published || doc.published == false)) {
+    emit([doc.lang, doc.type, doc.published], null);
+  }
+}
+
+// View name v_templates
+function (doc, meta) {
+  if (meta.type == "json" && doc.lang && doc.type && (doc.published || doc.published == false)) {
+    emit([doc.lang, doc.type, doc.published], null);
+  }
+}
